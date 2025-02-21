531f4668fb9bfc4176b32af5b44990fdd5fb2d11
YSW <56628542+plzCode@users.noreply.github.com>
2025-02-21 10:42:24 +0900

    study3

--- Like_Lion/Like_Lion/Program.cs
+++ Like_Lion/Like_Lion/Program.cs
@@ -12,11 +12,36 @@ namespace Like_Lion
         {
             /*Console.WriteLine("깃 허브 연결하기");*/
 
-            //변수 선언
-            int age;
-            age = 25;
+            /*
+                        //변수 선언
+                        int age;
+                        age = 25;
 
-            Console.WriteLine(age);
+                        Console.WriteLine(age);
+            */
+
+            /*
+                        //리터럴 : 코드에서 고정된 값을 의미
+                        int number = 10; //정수
+                        double pi = 3.141592653589793238462643383; //실수
+                        char letter = '한'; //문자
+                        string name = "Tarnished"; //문자열
+
+                        Console.WriteLine(number);
+                        Console.WriteLine(pi);
+                        Console.WriteLine(letter);
+                        Console.WriteLine(name);
+            */
+
+            float hp = 100.0f;
+            float att = 56.7f;
+            string name = "Tarnished";
+            char grade = 'S';
+
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
         }
     }
 }
